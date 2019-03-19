12a5feabd7bdf5d159d4583c9d802e1661c5906e
Josh Beard <joshuabeard92@gmail.com>
2019-03-19 19:52:12 -0400

    update example for clarity

--- youtill/examples/deprecated.py
+++ youtill/examples/deprecated.py
@@ -5,6 +5,10 @@ from youtill import deprecated
 def deprec_print():
     print('this is my deprecated function')
 
+def not_deprecated_print():
+    print('this is not a deprecated function')
+
 
 if __name__ == "__main__":
+    not_deprecated_print()
     deprec_print()
